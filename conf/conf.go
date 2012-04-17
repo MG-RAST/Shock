00f7da3133bf9c84852bd023c1918b6cb7d5d168
Jared Wilkening <jared.wilkening@gmail.com>
2012-04-17 09:21:26 -0500

    added conf var for email for resources req

--- conf/conf.go
+++ conf/conf.go
@@ -7,11 +7,12 @@ import (
 
 // Command line options
 var (
-	PORT      = flag.Int("port", 8000, "port to listen on")
-	DATAROOT  = flag.String("data", "", "data directory to store on disk files")
-	MONGODB   = flag.String("mongodb", "localhost", "hostname(s) of mongodb")
-	SECRETKEY = flag.String("secretkey", "supersupersecret", "secret key")
-	SITEPATH  = os.ExpandEnv("$GOPATH") + "/src/github.com/MG-RAST/Shock/site"
+	PORT       = flag.Int("port", 8000, "port to listen on")
+	DATAROOT   = flag.String("data", "", "data directory to store on disk files")
+	MONGODB    = flag.String("mongodb", "localhost", "hostname(s) of mongodb")
+	SECRETKEY  = flag.String("secretkey", "supersupersecret", "secret key")
+	SITEPATH   = os.ExpandEnv("$GOPATH") + "/src/github.com/MG-RAST/Shock/site"
+	ADMINEMAIL = flag.String("email", "admin@host.com", "server admin email")
 )
 
 func init() {
