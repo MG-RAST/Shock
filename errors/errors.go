165eb41430975a6fa56d38b7918cc62aa92d3c3b
Jared Wilkening <jared.wilkening@gmail.com>
2012-07-23 14:57:02 -0500

    added store/node.go error messages

--- errors/errors.go
+++ errors/errors.go
@@ -12,6 +12,8 @@ const (
 	MongoDocNotFound         = "Document not found"
 	UnAuth                   = "User Unauthorized"
 	NoAuth                   = "No Authorization"
+	AttrImut                 = "node attributes immutable"
+	FileImut                 = "node file immutable"
 	InvalidIndex             = "Invalid Index"
 	InvalidFileTypeForFilter = "Invalid file type for filter"
 )
