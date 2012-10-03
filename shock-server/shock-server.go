4cacdc11d9cf9b037d1b03047d66a49612c12b40
Jared Wilkening <jared.wilkening@gmail.com>
2012-10-03 14:21:27 +0200

    all error to error log, improved conf print

--- shock-server/shock-server.go
+++ shock-server/shock-server.go
@@ -21,6 +21,7 @@ func launchSite(control chan int, port int) {
 	err := goweb.ListenAndServeRoutes(fmt.Sprintf(":%d", conf.SITE_PORT), r)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: site: %v\n", err)
+		log.Error("ERROR: site: " + err.Error())
 	}
 	control <- 1 //we are ending
 }
@@ -34,28 +35,19 @@ func launchAPI(control chan int, port int) {
 	err := goweb.ListenAndServeRoutes(fmt.Sprintf(":%d", port), r)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: api: %v\n", err)
+		log.Error("ERROR: api: " + err.Error())
 	}
 	control <- 1 //we are ending
 }
 
 func main() {
-	fmt.Printf("%s\n######### Conf #########\ndata-root:\t%s\naccess-log:\t%s\nerror-log:\t%s\nmongodb:\t%s\nsecretkey:\t%s\nsite-port:\t%d\napi-port:\t%d\n\n####### Anonymous ######\nread:\t%t\nwrite:\t%t\ncreate-user:\t%t\n\n",
-		logo,
-		conf.DATA_PATH,
-		conf.LOGS_PATH+"/access.log",
-		conf.LOGS_PATH+"/error.log",
-		conf.MONGODB,
-		conf.SECRET_KEY,
-		conf.SITE_PORT,
-		conf.API_PORT,
-		conf.ANON_READ,
-		conf.ANON_WRITE,
-		conf.ANON_CREATEUSER,
-	)
+	printLogo()
+	printConf()
 
 	if _, err := os.Stat(conf.DATA_PATH + "/temp"); err != nil && os.IsNotExist(err) {
 		if err := os.Mkdir(conf.DATA_PATH+"/temp", 0777); err != nil {
 			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			log.Error("ERROR: " + err.Error())
 		}
 	}
 
@@ -65,6 +57,7 @@ func main() {
 		err := reload(conf.RELOAD)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			log.Error("ERROR: " + err.Error())
 		}
 		fmt.Println("Done")
 	}
