3d2f08d8e6be936089c1fb359436495f0a9729a3
Jared Wilkening <jared.wilkening@gmail.com>
2013-06-18 16:04:20 -0400

    python shock client lib

---
+++ libs/python/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import shock
+
+setup(name='shock',
+      title=shock.__title__,
+      version=shock.__version__,
+      author=shock.__author__,
+      license=shock.__licence__,
+      download_url='https://github.com/MG-RAST/Shock',
+      packages=['shock'],
+     )
