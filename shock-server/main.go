1f29b2f2f6e80306ac1ec8f27017e721968cfc74
Jared Bischof <jbischof@gmail.com>
2014-01-07 11:42:23 -0600

    Do not want long transfers to die.  Turning off read/write timeouts.

--- shock-server/main.go
+++ shock-server/main.go
@@ -188,8 +188,8 @@ func main() {
 	s := &http.Server{
 		Addr:           ":" + Address,
 		Handler:        goweb.DefaultHttpHandler(),
-		ReadTimeout:    100 * time.Second,
-		WriteTimeout:   100 * time.Second,
+		ReadTimeout:    0,
+		WriteTimeout:   0,
 		MaxHeaderBytes: 1 << 20,
 	}
 
