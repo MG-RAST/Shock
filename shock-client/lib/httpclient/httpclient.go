eb46523d118133f7a4e20b7199b1b589ad2dd8db
Jared Bischof <jbischof@gmail.com>
2014-03-10 16:13:22 -0500

    Moved httpclient lib back into Shock repo because it has settings specific to Shock and is not used anywhere else.

---
+++ shock-client/lib/httpclient/httpclient.go
@@ -0,0 +1,66 @@
+package httpclient
+
+import (
+	"crypto/tls"
+	"io"
+	"net/http"
+)
+
+type Header map[string]string
+type Auth struct {
+	Type     string
+	Username string
+	Password string
+	Token    string
+}
+
+var (
+	user *Auth = nil
+)
+
+func newTransport() *http.Transport {
+	return &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+}
+
+func SetTokenAuth(token string) {
+	user = &Auth{Type: "token", Token: token}
+}
+
+func SetBasicAuth(username, password string) {
+	user = &Auth{Type: "basic", Username: username, Password: password}
+}
+
+func Do(t string, url string, header Header, data io.Reader) (*http.Response, error) {
+	trans := newTransport()
+	req, err := http.NewRequest(t, url, data)
+	if err != nil {
+		return nil, err
+	}
+	if user != nil {
+		if user.Type == "basic" {
+			req.SetBasicAuth(user.Username, user.Password)
+		} else {
+			req.Header.Add("Authorization", "OAuth "+user.Token)
+		}
+	}
+	for k, v := range header {
+		req.Header.Add(k, v)
+	}
+	return trans.RoundTrip(req)
+}
+
+func Get(url string, header Header, data io.Reader) (resp *http.Response, err error) {
+	return Do("GET", url, header, data)
+}
+
+func Post(url string, header Header, data io.Reader) (resp *http.Response, err error) {
+	return Do("POST", url, header, data)
+}
+
+func Put(url string, header Header, data io.Reader) (resp *http.Response, err error) {
+	return Do("PUT", url, header, data)
+}
+
+func Delete(url string, header Header, data io.Reader) (resp *http.Response, err error) {
+	return Do("DELETE", url, header, data)
+}
