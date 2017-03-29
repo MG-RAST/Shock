569927f21c94c3a956e4ab4dedbbee6c45c2b6af
Travis Harrison <guumby@gmail.com>
2017-03-29 13:05:47 -0500

    fix index field type

--- shock-server/node/db.go
+++ shock-server/node/db.go
@@ -28,7 +28,7 @@ func Initialize() {
 	c.EnsureIndex(mgo.Index{Key: []string{"created_on"}, Background: true})
 	c.EnsureIndex(mgo.Index{Key: []string{"expiration"}, Background: true})
 	c.EnsureIndex(mgo.Index{Key: []string{"type"}, Background: true})
-	c.EnsureIndex(mgo.Index{Key: []int{"priority"}, Background: true})
+	c.EnsureIndex(mgo.Index{Key: []string{"priority"}, Background: true})
 	c.EnsureIndex(mgo.Index{Key: []string{"file.path"}, Background: true})
 	c.EnsureIndex(mgo.Index{Key: []string{"file.virtual_parts"}, Background: true})
 	c.EnsureIndex(mgo.Index{Key: []string{"file.checksum.md5"}, Background: true})
