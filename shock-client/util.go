3115b5610f0ca65941239e901d412dfdd2db1476
Travis Harrison <guumby@gmail.com>
2018-09-04 14:29:20 -0500

    new client

---
+++ shock-client/util.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const USAGE = `Usage: shock-client <command> [options...] [args..]
+Global Options:
+    --shock_url                 URL of shock-server, or env SHOCK_URL
+    --token                     User token for authorization, or env TOKEN
+    --bearer                    Bearer token, default is 'mgrast', or env BEARER
+
+Commands:
+
+help                            This help message
+
+info
+    Note: returns shock-server info from base url
+
+create [options...]
+    --attributes=<p>            JSON formated attribute file
+                                Note: Attributes will replace all current attributes
+    --chunk=<i>                 Upload in chunks, able to resume if disconnected
+                                Note: for --filepath option only
+    --filename=<s>              Filename to use, default is upload-file basename
+    --expiration=<s>            Epiration time: [0-9]+[MHD]
+    --compressed=<gzip|bzip2>   Uncompress after upload
+    --archive=<tar|zip>         Unpack after upload
+    
+    Mutualy exclusive options:
+    --filepath=<p>              Path to file
+    --part=<s>                  Number of parts to be uploaded
+    --virtual=<s>               Comma seperated list of node ids
+    --remote=<p>                URI of remote file
+    --copy=<id>                 ID of node to copy
+
+update [options...] <id>
+    Note: options are the same as create with additional below
+    --unexpire                  Remove expiration
+    --part=<s> --filepath=<p>   The part number to be uploaded and path to file
+                                Note: parts must be set
+
+index [options...] <id> <line|column|record|chunkrecord|bai>
+    --force                     Forces index to be rebuilt if exists
+    --column=<i>                Column number to index by if using 'column' type
+
+delete <id>
+
+get <id> [<output>]
+    Note: if output is not present the result will be written to stdout.
+
+query [<output>]
+    Note: if output is not present the results will be written to stdout.
+          use 
+    --format=<json|table>       Output JSON ot tabbed table
+    --limit=<i>                 Max number of output nodes, default 25
+    --offset=<i>                Position to start rerieving nodes at, default 0
+    --order=<s>                 Field to sort nodes by, default 'created_on'
+    --direction=<asc|desc>      Direction to sort by, default 'desc'
+    --attributes=<s>            Attribute field and value to search with
+                                Note: use "FIELD_NAME:VALUE"
+    --other=<s>                 Any non-attributes node field and value to search with
+    --distinct=<s>              An attribute field name, returns all unique values in that field
+
+download [options...] <id> [<output>]
+    Note: if output is not present the download will be written to stdout.
+    --md5                       Create md5 checksum of downloaded file
+    --index=<s>                 Name of index (must be used with -parts)
+    --part={s}                  Part(s) from index, may be a range eg. 1-10
+
+acl <get|add|delete> <all|read|write|delete> <id> <users>
+    Note: users are in the form of comma delimited list of user-names or uuids,
+          not required for get action
+
+public <add|delete> <all|read|write|delete> <id>
+
+chown <id> <user>
+    Note: user is user-name or uuid 
+
+`
+
+func exitHelp() {
+	fmt.Fprintln(os.Stdout, USAGE)
+	os.Exit(0)
+}
+
+func exitError(msg string) {
+	fmt.Fprintln(os.Stderr, USAGE)
+	if msg != "" {
+		fmt.Fprintln(os.Stderr, "Error: "+msg)
+	}
+	os.Exit(1)
+}
+
+func setFlags() (flags *flag.FlagSet) {
+	flags = flag.NewFlagSet("shockclient", flag.ContinueOnError)
+	flags.StringVar(&archive, "archive", "", "")
+	flags.StringVar(&attributes, "attributes", "", "")
+	flags.StringVar(&bearer, "bearer", "mgrast", "")
+	flags.IntVar(&chunk, "chunk", 0, "")
+	flags.IntVar(&column, "column", 0, "")
+	flags.StringVar(&compressed, "compressed", "", "")
+	flags.StringVar(&copy, "copy", "", "")
+	flags.StringVar(&direction, "direction", "", "")
+	flags.StringVar(&expiration, "expiration", "", "")
+	flags.StringVar(&filename, "filename", "", "")
+	flags.StringVar(&filepath, "filepath", "", "")
+	flags.BoolVar(&force, "force", false, "")
+	flags.StringVar(&format, "format", "", "")
+	flags.StringVar(&index, "index", "", "")
+	flags.IntVar(&limit, "limit", 0, "")
+	flags.BoolVar(&md5, "md5", false, "")
+	flags.IntVar(&offset, "offset", 0, "")
+	flags.StringVar(&order, "order", "", "")
+	flags.StringVar(&part, "part", "", "")
+	flags.StringVar(&remote, "remote", "", "")
+	flags.StringVar(&token, "token", "", "")
+	flags.BoolVar(&unexpire, "unexpire", false, "")
+	flags.StringVar(&url, "url", "", "")
+	flags.StringVar(&virtual, "virtual", "", "")
+	return
+}
+
+func getUserInfo() (url string, auth string) {
+	// set from env if exists
+	if shock_url == "" {
+		shock_url = os.Getenv("SHOCK_URL")
+	}
+	if token == "" {
+		token = os.Getenv("TOKEN")
+	}
+	if os.Getenv("BEARER") != "" {
+		bearer = os.Getenv("BEARER")
+	}
+	// test and return
+	if token != "" {
+		auth = bearer + " " + token
+	}
+	if shock_url == "" {
+		exitError("missing required --shock_url")
+	}
+	url = shock_url
+	return
+}
