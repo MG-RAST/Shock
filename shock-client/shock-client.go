2e89239d30cae941087be9bca55e508c2ff71a0b
Travis Harrison <guumby@gmail.com>
2018-09-06 16:23:43 -0500

    more functionality

--- shock-client/shock-client.go
+++ shock-client/shock-client.go
@@ -4,7 +4,10 @@ import (
 	"flag"
 	"fmt"
 	sc "github.com/MG-RAST/go-shock-client"
+	"io"
+	"net/url"
 	"os"
+	"strconv"
 )
 
 var (
@@ -22,6 +25,7 @@ var (
 	filepath   string
 	force      bool
 	index      string
+	length     int
 	limit      int
 	md5        bool
 	offset     int
@@ -30,6 +34,7 @@ var (
 	part       string
 	pretty     bool
 	remote     string
+	seek       int
 	shock_url  string
 	token      string
 	unexpire   bool
@@ -38,6 +43,32 @@ var (
 
 func stub(x string) {
 	fmt.Println("not implamented: " + x)
+}
+
+func buildDownloadUrl(host string, id string) string {
+	var query url.Values
+	query.Add("download", "")
+
+	if (index != "") && (part != "") {
+		if !validateCV("index", index) {
+			exitError("invalid index type")
+		}
+		query.Add("index", index)
+		query.Add("part", part)
+	} else if (seek > -1) && (length > 0) {
+		query.Add("seek", strconv.Itoa(seek))
+		query.Add("length", strconv.Itoa(length))
+	}
+
+	var myurl *url.URL
+	myurl, err := url.ParseRequestURI(host)
+	if err != nil {
+		exitError("error parsing shock url")
+	}
+	(*myurl).Path = "/node/" + id
+	(*myurl).RawQuery = query.Encode()
+
+	return myurl.String()
 }
 
 func main() {
@@ -51,10 +82,10 @@ func main() {
 
 	flags = setFlags()
 	flags.Parse(os.Args[2:])
-	//args := flags.Args()
+	args := flags.Args()
 
-	url, auth := getUserInfo()
-	client := sc.NewShockClient(url, auth, true)
+	host, auth := getUserInfo()
+	client := sc.NewShockClient(host, auth, true)
 
 	switch command {
 	case "help":
@@ -68,20 +99,85 @@ func main() {
 	case "create", "update":
 		stub(command)
 	case "index":
-		stub(command)
+		if len(args) < 2 {
+			exitError("missing required arguments")
+		}
+		if !validateCV("index", args[1]) {
+			exitError("invalid index type")
+		}
+		if (args[1] == "column") && (column < 1) {
+			exitError("invalid column position")
+		}
+		err := client.PutIndexQuery(args[0], args[1], force, column)
+		if err != nil {
+			exitError(err.Error())
+		}
+		os.Exit(0)
 	case "delete":
-		stub(command)
+		if len(args) < 1 {
+			exitError("missing required ID")
+		}
+		err := client.DeleteNode(args[0])
+		if err != nil {
+			exitError(err.Error())
+		}
+		os.Exit(0)
 	case "get":
-		stub(command)
+		if len(args) < 1 {
+			exitError("missing required ID")
+		}
+		node, err := client.GetNode(args[0])
+		if err != nil {
+			exitError(err.Error())
+		}
+		exitOutput(&node)
 	case "query":
 		stub(command)
 	case "download":
+		if len(args) < 1 {
+			exitError("missing required ID")
+		}
+		downUrl := buildDownloadUrl(host, args[0])
+		if (output == "") || (output == "-") || (output == "stdout") {
+			body, err := sc.FetchShockStream(downUrl, auth)
+			if err != nil {
+				exitError(err.Error())
+			}
+			defer body.Close()
+			_, err = io.Copy(os.Stdout, body)
+			if err != nil {
+				exitError(err.Error())
+			}
+		} else {
+			size, md5sum, err := sc.FetchFile(output, downUrl, auth, "", md5)
+			if err != nil {
+				exitError(err.Error())
+			}
+			fmt.Printf("download complete\nfile\t%s\nsize\t%d\nmd5\t%s\n", output, size, md5sum)
+		}
+		os.Exit(0)
+	case "acl":
 		stub(command)
-	case "acl", "public", "chown":
-		stub(command)
+	case "public":
+		if len(args) < 1 {
+			exitError("missing required ID")
+		}
+		err := client.MakePublic(args[0])
+		if err != nil {
+			exitError(err.Error())
+		}
+		os.Exit(0)
+	case "chown":
+		if len(args) < 2 {
+			exitError("missing required arguments")
+		}
+		err := client.ChownNode(args[0], args[1])
+		if err != nil {
+			exitError(err.Error())
+		}
+		os.Exit(0)
 	default:
 		exitError("invalid command: " + command)
 	}
 
 }
-
