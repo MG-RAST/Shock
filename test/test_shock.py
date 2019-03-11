baa52dcd38127369c68a3f0b6a7352474d951a57
Andreas Wilke <wilke@anl.gov>
2019-03-11 10:09:07 -0500

    Added get shock url and port from env

--- test/test_shock.py
+++ test/test_shock.py
@@ -3,13 +3,16 @@ from __future__ import print_function
 from os.path import dirname, abspath
 from subprocess import check_output
 import json
+import os
 import requests
 
 DATADIR = dirname(abspath(__file__)) + "/testdata/"
 DEBUG = 1
-PORT = "7445"
-SHOCK_URL = "http://localhost:"+PORT
+PORT = os.environ.get('SHOCK_PORT' , "7445")
+URL  = os.environ.get('SHOCK_HOST' , "http://localhost") 
+SHOCK_URL = URL + ":" + PORT
 TOKEN = "1234"
+
 
 def create_three_nodes():
     NODES = []
@@ -19,41 +22,53 @@ def create_three_nodes():
 # and passed as the files= parameter to requests.put
     TESTDATA = {"attributes_str": (None, '{"project_id":"TESTPROJECT"}')}
     FILES = {'upload': open(DATADIR + 'AAA.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, TESTDATA, TESTHEADERS)
+    if DEBUG:
+        print("POST", TESTURL, TESTDATA, TESTHEADERS)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     assert data["status"] == 200
     NODES += [data["data"]["id"]]
-    if DEBUG: print("PUT", SHOCK_URL+ "/node/" + NODES[-1], TESTDATA)
-    r = requests.put(SHOCK_URL + "/node/" + NODES[-1], files=TESTDATA, headers=TESTHEADERS)
+    if DEBUG:
+        print("PUT", SHOCK_URL + "/node/" + NODES[-1], TESTDATA)
+    r = requests.put(SHOCK_URL + "/node/" +
+                     NODES[-1], files=TESTDATA, headers=TESTHEADERS)
     FILES = {'upload': open(DATADIR + 'BBB.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, FILES)
+    if DEBUG:
+        print("POST", TESTURL, FILES)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     NODES += [data["data"]["id"]]
-    if DEBUG: print("PUT", SHOCK_URL+ "/node/" + NODES[-1], TESTDATA)
-    r = requests.put(SHOCK_URL + "/node/" + NODES[-1], files=TESTDATA, headers=TESTHEADERS)
+    if DEBUG:
+        print("PUT", SHOCK_URL + "/node/" + NODES[-1], TESTDATA)
+    r = requests.put(SHOCK_URL + "/node/" +
+                     NODES[-1], files=TESTDATA, headers=TESTHEADERS)
     FILES = {'upload': open(DATADIR + 'CCC.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, FILES)
+    if DEBUG:
+        print("POST", TESTURL, FILES)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     NODES += [data["data"]["id"]]
-    if DEBUG: print("PUT", SHOCK_URL+ "/node/" + NODES[-1], TESTDATA)
-    r = requests.put(SHOCK_URL + "/node/" + NODES[-1], files=TESTDATA, headers=TESTHEADERS)
+    if DEBUG:
+        print("PUT", SHOCK_URL + "/node/" + NODES[-1], TESTDATA)
+    r = requests.put(SHOCK_URL + "/node/" +
+                     NODES[-1], files=TESTDATA, headers=TESTHEADERS)
     print(r.content.decode("utf-8"))
     data = json.loads(r.content.decode("utf-8"))
     assert data["data"]["attributes"]["project_id"] == "TESTPROJECT"
     return(NODES)
 
+
 def confirm_nodes_project(NODES, PROJECT):
-     for NODEID in NODES:
+    for NODEID in NODES:
         TESTURL = SHOCK_URL + "/node/{}".format(NODEID)
         TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
-        if DEBUG: print("curl '{}' -H 'Authorization: Oauth {}'".format(TESTURL, TOKEN))
+        if DEBUG:
+            print("curl '{}' -H 'Authorization: Oauth {}'".format(TESTURL, TOKEN))
         response = requests.get(TESTURL, headers=TESTHEADERS)
         data = json.loads(response.content.decode("utf-8"))
         assert data["status"] == 200
         assert PROJECT in data["data"]["attributes"]["project_id"]
+
 
 def delete_nodes(NODELIST):
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
@@ -62,6 +77,7 @@ def delete_nodes(NODELIST):
         response = requests.delete(NODEURL, headers=TESTHEADERS)
         assert json.loads(response.content.decode("utf-8"))["status"] == 200
     return
+
 
 def test_nodelist_noauth():
     TESTURL = "{SHOCK_URL}/node/?query".format(SHOCK_URL=SHOCK_URL)
@@ -73,6 +89,7 @@ def test_nodelist_noauth():
     data = json.loads(response.content.decode("utf-8"))
     assert data["status"] == 200
 
+
 def test_nodelist_auth():
     TESTURL = "{SHOCK_URL}/node/?query".format(SHOCK_URL=SHOCK_URL)
     TESTDATA = {}
@@ -83,6 +100,7 @@ def test_nodelist_auth():
     data = json.loads(response.content.decode("utf-8"))
     assert data["status"] == 200
 
+
 def test_nodelist_badauth():
     TESTURL = "{SHOCK_URL}/node/?query".format(SHOCK_URL=SHOCK_URL)
     TESTDATA = {}
@@ -91,7 +109,9 @@ def test_nodelist_badauth():
         print(TESTURL, TESTDATA, TESTHEADERS)
     response = requests.get(TESTURL, headers=TESTHEADERS, params=TESTDATA)
     data = json.loads(response.content.decode("utf-8"))
-    assert data["status"] == 403 or data["status"] == 400  # 403 unauthorized 400 bad query
+    # 403 unauthorized 400 bad query
+    assert data["status"] == 403 or data["status"] == 400
+
 
 def test_upload_emptyfile():
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
@@ -108,6 +128,7 @@ def test_upload_emptyfile():
     NODEID = data["data"]["id"]
     NODEURL = SHOCK_URL + "/node/{}".format(NODEID)
     response = requests.delete(NODEURL, headers=TESTHEADERS)
+
 
 def test_upload_threefiles():
     NODES = create_three_nodes()
@@ -127,16 +148,19 @@ def test_upload_threefiles():
         NODEURL = SHOCK_URL + "/node/{}".format(NODEID)
         response = requests.delete(NODEURL, headers=TESTHEADERS)
 
+
 def test_upload_and_delete_node():
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {'upload': open(DATADIR + 'CCC.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, TESTHEADERS, FILES)
+    if DEBUG:
+        print("POST", TESTURL, TESTHEADERS, FILES)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     NODEID = data["data"]["id"]
     # test my node exists
-    if DEBUG: print("GET", TESTURL, TESTHEADERS)
+    if DEBUG:
+        print("GET", TESTURL, TESTHEADERS)
     TESTURL = SHOCK_URL + "/node/{}".format(NODEID)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {}
@@ -144,29 +168,34 @@ def test_upload_and_delete_node():
     data = json.loads(response.content.decode("utf-8"))
     assert data["status"] == 200
    # delete my node
-    if DEBUG: print("DELETE", TESTURL, TESTHEADERS)
+    if DEBUG:
+        print("DELETE", TESTURL, TESTHEADERS)
     TESTURL = SHOCK_URL+"/node/{}".format(NODEID)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     response = requests.delete(TESTURL, headers=TESTHEADERS)
     data = json.loads(response.content.decode("utf-8"))
     # test my node is gone
-    if DEBUG: print("GET", TESTURL, TESTHEADERS)
+    if DEBUG:
+        print("GET", TESTURL, TESTHEADERS)
     TESTURL = SHOCK_URL + "/node/{}".format(NODEID)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     response = requests.get(TESTURL, headers=TESTHEADERS)
     data = json.loads(response.content.decode("utf-8"))
     assert data["status"] == 404
 
+
 def test_upload_and_download_node_GET():
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {'upload': open(DATADIR + 'CCC.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, TESTHEADERS, FILES)
+    if DEBUG:
+        print("POST", TESTURL, TESTHEADERS, FILES)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     NODEID = data["data"]["id"]
     # test my node exists
-    if DEBUG: print("GET", TESTURL, TESTHEADERS)
+    if DEBUG:
+        print("GET", TESTURL, TESTHEADERS)
     TESTURL = SHOCK_URL + "/node/{}".format(NODEID)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {}
@@ -176,9 +205,10 @@ def test_upload_and_download_node_GET():
     DLURL = SHOCK_URL + "/node/{}?download".format(NODEID)
     response = requests.get(DLURL, headers=TESTHEADERS)
     assert response.content[0:3] == b"CCC"
-    #cleanup
+    # cleanup
     NODEURL = SHOCK_URL + "/node/{}".format(NODEID)
     requests.delete(NODEURL, headers=TESTHEADERS)
+
 
 def test_upload_and_download_node_GET_gzip():
     # download file in compressed format, works with all the above options
@@ -187,7 +217,8 @@ def test_upload_and_download_node_GET_gzip():
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {'upload': open(DATADIR + 'CCC.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, TESTHEADERS, FILES)
+    if DEBUG:
+        print("POST", TESTURL, TESTHEADERS, FILES)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     NODEID = data["data"]["id"]
@@ -195,7 +226,8 @@ def test_upload_and_download_node_GET_gzip():
     TESTURL = SHOCK_URL + "/node/{}".format(NODEID)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {}
-    if DEBUG: print("GET", TESTURL, TESTHEADERS)
+    if DEBUG:
+        print("GET", TESTURL, TESTHEADERS)
     response = requests.get(TESTURL, headers=TESTHEADERS)
     data = json.loads(response.content.decode("utf-8"))
     assert data["status"] == 200
@@ -203,20 +235,23 @@ def test_upload_and_download_node_GET_gzip():
     DLURL = SHOCK_URL + "/node/{}?download&compression=gzip".format(NODEID)
     response = requests.get(DLURL, headers=TESTHEADERS)
     assert response.content[0:3] != b"CCC"
-    #cleanup
+    # cleanup
     NODEURL = SHOCK_URL + "/node/{}".format(NODEID)
     requests.delete(NODEURL, headers=TESTHEADERS)
+
 
 def test_upload_and_download_node_GET_zip():
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {'upload': open(DATADIR + 'CCC.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, TESTHEADERS, FILES)
+    if DEBUG:
+        print("POST", TESTURL, TESTHEADERS, FILES)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     NODEID = data["data"]["id"]
     # test my node exists
-    if DEBUG: print("GET", TESTURL, TESTHEADERS)
+    if DEBUG:
+        print("GET", TESTURL, TESTHEADERS)
     TESTURL = SHOCK_URL + "/node/{}".format(NODEID)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {}
@@ -226,41 +261,46 @@ def test_upload_and_download_node_GET_zip():
     DLURL = SHOCK_URL + "/node/{}?download&compression=zip".format(NODEID)
     response = requests.get(DLURL, headers=TESTHEADERS)
     assert response.content[0:3] != b"CCC"
-    #cleanup
+    # cleanup
     NODEURL = SHOCK_URL + "/node/{}".format(NODEID)
     requests.delete(NODEURL, headers=TESTHEADERS)
+
 
 def test_upload_and_download_node_gzip():
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
     FILES = {'upload': open(DATADIR + 'CCC.txt', 'rb')}
-    if DEBUG: print("POST", TESTURL, TESTHEADERS, FILES)
+    if DEBUG:
+        print("POST", TESTURL, TESTHEADERS, FILES)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=FILES)
     data = json.loads(response.content.decode("utf-8"))
     NODEID = data["data"]["id"]
     # test my node exists
     TESTURL = SHOCK_URL + "/node/{}".format(NODEID)
     FILES = {}
-    if DEBUG: print("GET", TESTURL, TESTHEADERS)
+    if DEBUG:
+        print("GET", TESTURL, TESTHEADERS)
     response = requests.get(TESTURL, headers=TESTHEADERS)
     data = json.loads(response.content.decode("utf-8"))
     assert data["status"] == 200
     DLURL = SHOCK_URL + "/node/{}?download&compression=gzip".format(NODEID)
     response = requests.get(DLURL, headers=TESTHEADERS)
     assert response.content[0:3] != b"CCC"
-    #cleanup
+    # cleanup
     NODEURL = SHOCK_URL + "/node/{}".format(NODEID)
     requests.delete(NODEURL, headers=TESTHEADERS)
 
+
 def test_download_zip_GET():
     NODES = create_three_nodes()
-    #confirm nodes exist
+    # confirm nodes exist
     confirm_nodes_project(NODES, "TESTPROJECT")
     # query for TESTDATA
     TESTURL = "{SHOCK_URL}/node?query".format(SHOCK_URL=SHOCK_URL)
     TESTDATA = {"project_id": "TESTPROJECT"}
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
-    if DEBUG: print("GET", TESTURL, TESTDATA)
+    if DEBUG:
+        print("GET", TESTURL, TESTDATA)
     response = requests.get(TESTURL, headers=TESTHEADERS, params=TESTDATA)
 #    if DEBUG: print(response.content)
     data = json.loads(response.content.decode("utf-8"))
@@ -268,7 +308,8 @@ def test_download_zip_GET():
     assert NODES[0] in response.content.decode("utf-8")
     # issue query for TESTPROJECT FILES downloaded as ZIP
     TESTURL = SHOCK_URL+"/node?query&download_url&archive=zip".format()
-    if DEBUG: print("curl '{}' -H 'Authorization: Oauth {}' -G -d {}".format(TESTURL, TOKEN, TESTDATA))
+    if DEBUG:
+        print("curl '{}' -H 'Authorization: Oauth {}' -G -d {}".format(TESTURL, TOKEN, TESTDATA))
     response = requests.get(TESTURL, headers=TESTHEADERS, params=TESTDATA)
     data = json.loads(response.content.decode("utf-8"))
     # extract preauth uri from response
@@ -276,15 +317,16 @@ def test_download_zip_GET():
     response = requests.get(PREAUTH, headers=TESTHEADERS)
     # write it to file and test ZIP
     with open("TEST.zip", "wb") as F:
-         F.write(response.content)
+        F.write(response.content)
     out = check_output("unzip -l TEST.zip", shell=True)
     assert b'TEST.zip' in out
     assert b'CCC.txt' in out
-    assert b'     4 ' in out # This fails if there are no 4-byte-files
+    assert b'     4 ' in out  # This fails if there are no 4-byte-files
     # cleanup
     for NODEID in NODES:
         NODEURL = SHOCK_URL + "/node/{}".format(NODEID)
         response = requests.delete(NODEURL, headers=TESTHEADERS)
+
 
 def test_download_tar_GET():
     # Per test invokation on https://github.com/MG-RAST/Shock/wiki/API
@@ -293,13 +335,14 @@ def test_download_tar_GET():
     # curl -X GET http://<host>[:<port>]/node?query&download_url&archive=zip&<key>=<value>
 
     NODES = create_three_nodes()
-    #confirm nodes exist
+    # confirm nodes exist
     confirm_nodes_project(NODES, "TESTPROJECT")
     # query for TESTDATA
     TESTURL = "{SHOCK_URL}/node?query".format(SHOCK_URL=SHOCK_URL)
     TESTDATA = {"project_id": "TESTPROJECT"}
     TESTHEADERS = {"Authorization": "OAuth {}".format(TOKEN)}
-    if DEBUG: print("GET", TESTURL, TESTDATA)
+    if DEBUG:
+        print("GET", TESTURL, TESTDATA)
     response = requests.get(TESTURL, headers=TESTHEADERS, params=TESTDATA)
 #    if DEBUG: print(response.content)
     data = json.loads(response.content.decode("utf-8"))
@@ -307,7 +350,8 @@ def test_download_tar_GET():
     assert NODES[0] in response.content.decode("utf-8")
     # issue query for TESTPROJECT FILES downloaded as ZIP
     TESTURL = SHOCK_URL+"/node?query&download_url&archive=tar".format()
-    if DEBUG: print("curl '{}' -H 'Authorization: Oauth {}' -G -d {}".format(TESTURL, TOKEN, TESTDATA))
+    if DEBUG:
+        print("curl '{}' -H 'Authorization: Oauth {}' -G -d {}".format(TESTURL, TOKEN, TESTDATA))
     response = requests.get(TESTURL, headers=TESTHEADERS, params=TESTDATA)
     data = json.loads(response.content.decode("utf-8"))
     # extract preauth uri from response
@@ -322,9 +366,10 @@ def test_download_tar_GET():
     # cleanup
     delete_nodes(NODES)
 
+
 def test_download_tar_POST():
     NODES = create_three_nodes()
-    #confirm nodes exist
+    # confirm nodes exist
     confirm_nodes_project(NODES, "TESTPROJECT")
     # query for TESTDATA
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
@@ -334,28 +379,31 @@ def test_download_tar_POST():
                 "download_url": (None, 1),
                 "archive_format": (None, "tar")}
     # issue query for TESTPROJECT FILES downloaded as TAR
-    if DEBUG: print("POST", TESTURL, TESTDATA)
+    if DEBUG:
+        print("POST", TESTURL, TESTDATA)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=TESTDATA)
     data = json.loads(response.content.decode("utf-8"))
     # extract preauth uri from response
-    if DEBUG: print(response)
+    if DEBUG:
+        print(response)
     PREAUTH = data["data"]["url"]
     response = requests.get(PREAUTH, headers=TESTHEADERS)
     # write it to file and test ZIP
     with open("TESTP.tar", "wb") as f:
-         f.write(response.content)
+        f.write(response.content)
     out = check_output("tar tvf TESTP.tar", shell=True)
     assert b'CCC.txt' in out
     assert b'     4 ' in out  # This fails if there are no 4-byte-files
     # cleanup
     delete_nodes(NODES)
 
+
 def test_download_zip_POST():
     # Per test invokation on https://github.com/MG-RAST/Shock/wiki/API
     # use download_url with a POST and list of node ids
     # curl -X POST -F "download_url=1" -F "archive_format=zip" -F "ids=<node_id_1>,<node_id_2>,<...>" http://<host>[:<port>]/node
     NODES = create_three_nodes()
-    #confirm nodes exist
+    # confirm nodes exist
     confirm_nodes_project(NODES, "TESTPROJECT")
     # query for TESTDATA
     TESTURL = "{SHOCK_URL}/node".format(SHOCK_URL=SHOCK_URL)
@@ -365,16 +413,18 @@ def test_download_zip_POST():
                 "download_url": (None, 1),
                 "archive_format": (None, "zip")}
     # issue query for TESTPROJECT FILES downloaded as TAR
-    if DEBUG: print("POST", TESTURL, TESTDATA)
+    if DEBUG:
+        print("POST", TESTURL, TESTDATA)
     response = requests.post(TESTURL, headers=TESTHEADERS, files=TESTDATA)
     data = json.loads(response.content.decode("utf-8"))
     # extract preauth uri from response
-    if DEBUG: print(response)
+    if DEBUG:
+        print(response)
     PREAUTH = data["data"]["url"]
     response = requests.get(PREAUTH, headers=TESTHEADERS)
     # write it to file and test ZIP
     with open("TESTP.zip", "wb") as f:
-         f.write(response.content)
+        f.write(response.content)
     out = check_output("unzip -l TESTP.zip", shell=True)
     assert b'CCC.txt' in out
     assert b'     4 ' in out  # This fails if there are no 4-byte-files
