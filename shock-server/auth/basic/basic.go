9b10fecb70e59ed671251e5abe197f37a52f2e04
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2019-08-26 16:03:15 -0500

    enable basic auth

--- shock-server/auth/basic/basic.go
+++ shock-server/auth/basic/basic.go
@@ -5,16 +5,25 @@ package basic
 import (
 	"encoding/base64"
 	"errors"
+	"strings"
+
 	e "github.com/MG-RAST/Shock/shock-server/errors"
 	"github.com/MG-RAST/Shock/shock-server/user"
-	"strings"
 )
 
 // DecodeHeader takes the request authorization header and returns
 // username and password if it is correctly encoded.
 func DecodeHeader(header string) (username string, password string, err error) {
-	if strings.ToLower(strings.Split(header, " ")[0]) == "basic" {
-		if val, err := base64.URLEncoding.DecodeString(strings.Split(header, " ")[1]); err == nil {
+	headerArray := strings.Split(header, " ")
+	if len(headerArray) != 2 {
+		err = errors.New(e.InvalidAuth)
+		return
+	}
+	bearer := headerArray[0]
+	token := headerArray[1]
+	if strings.ToLower(bearer) == "basic" {
+
+		if val, err := base64.URLEncoding.DecodeString(token); err == nil {
 			tmp := strings.Split(string(val), ":")
 			if len(tmp) >= 2 {
 				return tmp[0], tmp[1], nil
@@ -32,7 +41,9 @@ func DecodeHeader(header string) (username string, password string, err error) {
 // Auth takes the request authorization header and returns
 // user
 func Auth(header string) (u *user.User, err error) {
+	//fmt.Printf("auth: %s\n", header)
 	username, password, err := DecodeHeader(header)
+	//fmt.Printf("auth: %s %s\n", username, password)
 	if err == nil {
 		return user.FindByUsernamePassword(username, password)
 	}
